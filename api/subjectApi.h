933944f02d2bbdf331703914eb6bf9d2be41c644
EnverPodgorcevic <enver.podgorcevic@fet.ba>
2017-06-24 14:35:25 +0200

    API klasa

--- api/subjectApi.h
+++ api/subjectApi.h
@@ -1,19 +1,20 @@
 #ifndef SUBJECTAPI_H
 #define SUBJECTAPI_H
 
-#include "./student.h"
-#include "./department.h"
 #include "./subject.h"
+#include "./dto/subjectDTO.h"
 #include <string>
+#include <stdexcept>
 
 
 class SubjectApi
 {
 private:
-	Subject S;	
+	Subject S;
+	SubjectApi a;
 
 public:
-	void save(int id, std::string , int ects, std::string abbreviation);
+	void save(int id, std::string , int, std::string);
 	/*
 	 *Metod treba da u file "db/subjects.txt" snimi predmet sa novim ID brojem.
 	 *Ukoliko department sa datim IDom vec postoji, javiti gresku.
@@ -28,12 +29,41 @@ public:
 	 *Metod treba da obrise predmet iz liste "db/subjects.txt" sa datim ID brojem.
 	 *Ukoliko ID ne postoji u listi, generirati gresku.
 	 */
-	void update(int id, std::string);
+	void update(int id, std::string, int, std::string);
 	/*
 	 *Metod treba da u fileu "db/subjects.txt" updateuje predmet sa vec postojecim ID brojem.
 	 *Ukoliko student sa datim IDom ne postoji, javiti gresku.
 	 */
 	
 };
+
+void SubjectApi::save(int id, std::string name, int ects, std::string abbreviation){
+	S.setId(id);
+	S.setName(name);
+	S.setEcts(ects);
+	S.setAbbr(abbreviation);
+	/*
+	 *Spajanje atributa u jedan objekat koji ce se proslijediti dalje
+	 */
+
+	a.save(S);
+}
+
+void SubjectApi::read(int id) const{
+	a.read(id);
+}
+
+void SubjectApi::del(int id){
+	a.del(id);
+}
+
+void SubjectApi::update(int id, std::string name, int ects, std::string abbreviation){
+	S.setId(id);
+	S.setName(name);
+	S.setEcts(ects);
+	S.setAbbr(abbreviation);
+
+	a.update(a);
+}
 
 #endif /* SUBJECTAPI_H */
