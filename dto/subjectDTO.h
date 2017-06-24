529b25353902fb0b66d18abcb7601eec01d676df
EnverPodgorcevic <enver.podgorcevic@fet.ba>
2017-06-24 16:07:29 +0200

    Data Transfer Object

---
+++ dto/subjectDTO.h
@@ -0,0 +1,18 @@
+#ifndef SUBJECTDTO_H
+#define SUBJECTDTO_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class SubjectDTO
+{
+private:
+	
+
+public:
+	SubjectDTOSubjectDTO();
+	virtual ~SubjectDTOSubjectDTO();
+};
+
+#endif /* SUBJECTDTO_H */
