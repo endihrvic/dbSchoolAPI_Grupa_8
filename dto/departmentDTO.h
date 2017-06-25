58b8209fc4d2e70612b890764a4cd61edee40ff5
EnverPodgorcevic <enver.podgorcevic@fet.ba>
2017-06-25 13:40:01 +0200

    Update departmentDTO.h

--- dto/departmentDTO.h
+++ dto/departmentDTO.h
@@ -37,7 +37,7 @@ Department strToSubject(std::string d){
 std::string subjectToStr(Department a){
 	std::string s;
 	std::stringstream ss;
-	ss << a.getId() << ", " << a.getName() << "\n";
+	ss << a.getId() << ", " << a.getName();
 	ss >> s;
 	return s;
 }
